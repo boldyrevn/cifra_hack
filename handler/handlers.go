a3b5b17eea0dea430c0cf9dfc1a39881d2f251a1
boldyrevn <boldyrev.now@mail.ru>
2023-10-14 14:43:30 +0300

    first endpoint is done

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,19 +1,36 @@
 package handler
 
 import (
-    "first_goland_project/crud"
-    "net/http"
+	"encoding/json"
+	"first_goland_project/crud"
+	"first_goland_project/model"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 type Service struct {
-    ds crud.Service
+	ds crud.Service
 }
 
 func GetService() Service {
-    hs := Service{crud.GetService()}
-    return hs
+	hs := Service{crud.GetService()}
+	return hs
 }
 
-func GetUser(rw http.ResponseWriter, r *http.Request) {
+func (s *Service) CloseConn() {
+	s.ds.CloseConn()
+}
 
+func (s *Service) GetUser(rw http.ResponseWriter, r *http.Request) {
+	email := mux.Vars(r)["email"]
+	u, err := s.ds.GetUser(email)
+	if err != nil {
+		m, _ := json.Marshal(model.ErrorMessage{Message: "user is not found"})
+		rw.WriteHeader(http.StatusBadRequest)
+		_, _ = rw.Write(m)
+		return
+	}
+	rw.WriteHeader(http.StatusOK)
+	m, _ := json.Marshal(u)
+	_, _ = rw.Write(m)
 }
--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-    "context"
-    "first_goland_project/crud"
-    "github.com/gorilla/mux"
-    "github.com/joho/godotenv"
-    "log"
+	"first_goland_project/handler"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
 
 func init() {
-    if err := godotenv.Load(); err != nil {
-        log.Fatal("unable to load .env variables")
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("unable to load .env variables")
+	}
 }
 
 func main() {
-    conn := crud.connectDb()
-    defer conn.Close(context.Background())
+	h := handler.GetService()
+	defer h.CloseConn()
 
-    router := mux.NewRouter()
-    router.HandleFunc("/user/{email}")
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{email}", h.GetUser).Methods("GET")
+	router.HandleFunc("/user", h.GetUser).Methods("POST")
+	log.Fatal(http.ListenAndServe(":9777", router))
 }
